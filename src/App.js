e975e19785b19a33654301dfd33683cfdc41768a
RidhaHaze <ridha.benrekhissa@gmail.com>
2023-03-08 14:31:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LandPage from './component/LandPage';
+import Map from './component/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <LandPage/>
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
